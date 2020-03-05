bee58d73f7113ea4f6ad53b986cb2d53f4072451
Lorenzo Soto <lorenzo.soto@gmail.com>
2020-03-05 14:31:57 +0100

    Moved to using node-shellies

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ class ShellyShutterPlatform {
     this.log = log;
     this.config = config;
     this.oldAccessories = new Map();
-    this.newAccessories = new Map();
     this.api = api;
     this.name = PLATFORM_NAME;
 
@@ -30,36 +29,20 @@ class ShellyShutterPlatform {
     const WindowCoverAccessory = (await import('./lib/WindowCoverAccessory.mjs')).default;
     const ShellyShutterManager = (await import('./lib/ShellyShutterManager.mjs')).default;
     const manager = new ShellyShutterManager(this.log, this.config.devices);
-    const devices = await manager.getDevices();
-    devices.forEach(device => new WindowCoverAccessory(this.log, this.api, this.accessoryFromDevice(device), device));
-    this.registerAccessories();
+    manager.on('discover', device => new WindowCoverAccessory(this.log, this.api, this.accessoryFromDevice(device), device));
+    manager.start();
   }
 
   accessoryFromDevice(device) {
     let accessory = this.oldAccessories.get(device.name);
     if (accessory) {
       this.log("Reusing accessory " + accessory.displayName);
-      this.oldAccessories.delete(device.name);
     } else {
       const Categories = this.api.hap.Accessory.Categories;
       accessory = new this.api.platformAccessory(device.name, this.api.hap.uuid.generate(device.name), Categories.WINDOW_COVERING);
+      this.api.registerPlatformAccessories(PLUGIN_NAME, this.name, [accessory]);
       this.log("Created accessory " + accessory.displayName);
-      this.newAccessories.set(device.name, accessory);
     }
     return accessory;
-  }
-
-  registerAccessories() {
-    if (this.oldAccessories.size > 0) {
-      this.log(this.name, "Unregistering cached accessories " + [...this.oldAccessories.keys()]);
-      this.api.unregisterPlatformAccessories(PLUGIN_NAME, this.name, [...this.oldAccessories.values()]);
-    }
-    delete this.oldAccessories;
-
-    if (this.newAccessories.size > 0) {
-      this.log(this.name, "Registering platform accessories " + [...this.newAccessories.keys()]);
-      this.api.registerPlatformAccessories(PLUGIN_NAME, this.name, [...this.newAccessories.values()]);
-    }
-    delete this.newAccessories;
   }
 }
