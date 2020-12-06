c940db2c3b6768be08dd7f4a87e740947c16887f
Lorenzo Soto <lorenzo.soto@gmail.com>
2020-12-06 20:29:02 +0100

    Added support for single shutter

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ class ShellyPlatform {
     const ShellyManager = (await import('./lib/ShellyManager.mjs')).default;
     const manager = new ShellyManager(this.log);
     manager.on('discover', this.createAccessory.bind(this));
-    manager.start();
+    manager.start(config.iface);
   }
 
   createAccessory(device, accessoryClass) {
